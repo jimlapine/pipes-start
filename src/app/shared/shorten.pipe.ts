05a38ed95f5b2d1cd1553249e5057bbff0451ed7
jimlapine <jim.lapine@gmail.com>
2017-09-01 17:01:15 -0400

    Add Parameter to pipe

--- src/app/shared/shorten.pipe.ts
+++ src/app/shared/shorten.pipe.ts
@@ -4,9 +4,12 @@ import { Pipe, PipeTransform } from '@angular/core';
   name: 'shorten'
 })
 export class ShortenPipe implements PipeTransform {
-  transform(value: any) {
-    if (value.length > 10) {
-      return value.substr(0, 10) + ' ...';
+  transform(value: any, limit: number) {
+    if (limit === undefined) {
+      limit = 10;
+    }
+    if (value.length > limit) {
+      return value.substr(0, limit) + ' ...';
     } else {
       return value;
     }
