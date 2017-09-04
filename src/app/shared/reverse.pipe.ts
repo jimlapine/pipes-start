5aa1ede9397ebdad5f6a42a693d42b7d5a76460e
jimlapine <jim.lapine@gmail.com>
2017-09-03 23:45:43 -0400

    simplfy filtter based on class example

--- src/app/shared/reverse.pipe.ts
+++ src/app/shared/reverse.pipe.ts
@@ -6,14 +6,6 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class ReversePipe implements PipeTransform {
 
   transform(value: any): any {
-    let retVal = '';
-    if (value.length === 0) {
-      return value;
-    } else {
-      for (let i = value.length - 1; i >= 0; i--) {
-        retVal += value[i];
-      }
-      return retVal;
-    }
+    return value.split('').reverse().join('');
   }
 }
